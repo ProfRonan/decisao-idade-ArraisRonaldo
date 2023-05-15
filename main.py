36177e03d33be8ef9300ae9eb164c708d8a62244
Ronan Pardo Soares <ronan.soares@gmail.com>
2023-05-15 15:59:06 -0300

    Fix change of tests

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 abc = int(input("digite sua idade: "))
 
 if abc < 0:
-    print("impossível!")
+    print("Impossível!")
 if abc < 18:
-        print("não precisa se alistar.")
+        print("Não precisa se alistar.")
 elif abc > 18 and abc < 65:
             print("Não esqueça de votar na próxima eleição.")
 elif abc > 65:
                 print("Vá descansar.")
 else:
-                print("eita!")
+                print("Eita!")
