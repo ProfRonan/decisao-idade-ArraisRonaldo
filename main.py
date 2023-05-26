744debf4ed06348e846eaa3f0b0f426c57868426
ArraisRonaldo <128387746+ArraisRonaldo@users.noreply.github.com>
2023-05-26 11:20:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ abc = int(input("digite sua idade: "))
 
 if abc < 0:
     print("Impossível!")
-if abc < 18:
+elif abc < 18:
         print("Não precisa se alistar.")
 elif abc > 18 and abc < 65:
             print("Não esqueça de votar na próxima eleição.")
